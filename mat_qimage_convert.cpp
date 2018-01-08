fda179c6fb6d831fb00fd03cff6632069f95d077
Wang Hongshuo <iswanghongshuo@foxmail.com>
2018-01-08 20:24:33 +0800

    daily update

--- mat_qimage_convert.cpp
+++ mat_qimage_convert.cpp
@@ -1,32 +1,35 @@
 #include "mat_qimage_convert.h"
+#include <QDebug>
 
-
-QImage Mat2QImage_with_pointer(cv::Mat &mat)
+// cv::Mat::data and QImage::bits() are the same
+QImage Mat2QImage_with_pointer(cv::Mat &src)
 {
-    // 单通道
-    if(mat.type() == CV_8UC1)
+    // 1 channel
+    if(src.type() == CV_8UC1)
     {
-        QImage image(mat.data,mat.cols, mat.rows,mat.step, QImage::Format_Indexed8);
+        QImage dst(src.data,src.cols, src.rows,src.step, QImage::Format_Indexed8);
 
-        image.setColorCount(256);
+        dst.setColorCount(256);
         for(int i = 0; i < 256; i++)
         {
-            image.setColor(i, qRgb(i, i, i));
+            dst.setColor(i, qRgb(i, i, i));
         }
-        return image;
+        return dst;
     }
-    // 3通道
-    else if(mat.type() == CV_8UC3)
+    // 3 channels
+    else if(src.type() == CV_8UC3)
     {
-        cv::cvtColor(mat,mat,CV_BGR2RGB);
-        QImage image(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
-        return image;
+        // warning: pay attention to this:
+        cv::cvtColor(src,src,CV_BGR2RGB);
+        // it will change BRG to RBG in src QImage
+        QImage dst(src.data, src.cols, src.rows, src.step, QImage::Format_RGB888);
+        return dst;
     }
-    // 4通道
-    else if(mat.type() == CV_8UC4)
+    // 4 channels
+    else if(src.type() == CV_8UC4)
     {
-        QImage image(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_ARGB32);
-        return image;
+        QImage dst(src.data, src.cols, src.rows, src.step, QImage::Format_ARGB32);
+        return dst;
     }
     else
     {
@@ -34,79 +37,93 @@ QImage Mat2QImage_with_pointer(cv::Mat &mat)
     }
 }
 
-cv::Mat QImage2Mat_with_pointer(QImage &image)
+// cv::Mat::data and QImage::bits() are the same
+cv::Mat QImage2Mat_with_pointer(QImage &src)
 {
-    cv::Mat mat;
-//    qDebug() << image.format();
-    switch(image.format())
+    cv::Mat dst;
+//    qDebug() << src.format();
+    switch(src.format())
     {
     case QImage::Format_ARGB32:
     case QImage::Format_RGB32:
     case QImage::Format_ARGB32_Premultiplied:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
+        dst = cv::Mat(src.height(), src.width(), CV_8UC4, (void*)src.constBits(), src.bytesPerLine());
         break;
     case QImage::Format_RGB888:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
-        cv::cvtColor(mat, mat, CV_BGR2RGB);
+        dst = cv::Mat(src.height(), src.width(), CV_8UC3, (void*)src.constBits(), src.bytesPerLine());
+        // warning: pay attention to this:
+        cv::cvtColor(dst, dst, CV_BGR2RGB);
+        // it will change BRG to RBG in src QImage
         break;
     case QImage::Format_Indexed8:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
+        dst = cv::Mat(src.height(), src.width(), CV_8UC1, (void*)src.constBits(), src.bytesPerLine());
         break;
     }
-    return mat;
+    return dst;
 }
 
-QImage Mat2QImage_with_data(cv::Mat &mat)
+// cv::Mat::data and QImage::bits() are different
+QImage Mat2QImage_with_data(cv::Mat &src)
 {
-    // 单通道
-    if(mat.type() == CV_8UC1)
+    // 1 channel
+    if(src.type() == CV_8UC1)
     {
-        QImage image(mat.data,mat.cols, mat.rows,mat.step, QImage::Format_Indexed8);
-
-        image.setColorCount(256);
+        QImage dst(src.data,src.cols, src.rows,src.step, QImage::Format_Indexed8);
+        dst.setColorCount(256);
         for(int i = 0; i < 256; i++)
         {
-            image.setColor(i, qRgb(i, i, i));
+            dst.setColor(i, qRgb(i, i, i));
         }
-        return image.copy();
+        return dst.copy();
     }
-    // 3通道
-    else if(mat.type() == CV_8UC3)
+    // 3 channels
+    else if(src.type() == CV_8UC3)
     {
         cv::Mat temp;
-        cv::cvtColor(mat,temp,CV_BGR2RGB);
-        QImage image(temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
-        return image.copy();
+        cv::cvtColor(src,temp,CV_BGR2RGB);
+        QImage dst(temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
+        return dst.copy();
     }
-    // 4通道
-    else if(mat.type() == CV_8UC4)
+    // 4 channels
+    else if(src.type() == CV_8UC4)
     {
-        QImage image(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_ARGB32);
-        return image.copy();
+        QImage dst(src.data, src.cols, src.rows, src.step, QImage::Format_ARGB32);
+        return dst.copy();
     }
     else
     {
         return QImage();
     }
 }
-cv::Mat QImage2Mat_with_data(QImage &image)
+
+// cv::Mat::data and QImage::bits() are different
+cv::Mat QImage2Mat_with_data(QImage &src)
 {
-    cv::Mat mat;
-//    qDebug() << image.format();
-    switch(image.format())
+    cv::Mat dst;
+//    qDebug() << src.format();
+    switch(src.format())
     {
+    // 4 channels
     case QImage::Format_ARGB32:
     case QImage::Format_RGB32:
     case QImage::Format_ARGB32_Premultiplied:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
-        break;
-    case QImage::Format_RGB888:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
-        cv::cvtColor(mat, mat, CV_BGR2RGB);
-        break;
-    case QImage::Format_Indexed8:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
-        break;
+    {
+        dst = cv::Mat(src.height(), src.width(), CV_8UC4, (void*)src.constBits(), src.bytesPerLine());
+        return dst.clone();
     }
-    return mat.clone();
+    // 3 channels
+    case QImage::Format_RGB888:
+    {
+        dst = cv::Mat(src.height(), src.width(), CV_8UC3, (void*)src.constBits(), src.bytesPerLine());
+        cv::Mat dstTemp;
+        cv::cvtColor(dst, dstTemp, CV_BGR2RGB);
+        return  dstTemp;
+    }
+    // 1 channel
+    case QImage::Format_Indexed8:
+    {
+        dst = cv::Mat(src.height(), src.width(), CV_8UC1, (void*)src.constBits(), src.bytesPerLine());
+        return dst.clone();
+    }
+    }
 }
--- mat_qimage_convert.h
+++ mat_qimage_convert.h
@@ -1,14 +1,23 @@
 #ifndef MAT_QIMAGE_CONVERT_H
 #define MAT_QIMAGE_CONVERT_H
 
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <QImage>
 
-QImage Mat2QImage_with_pointer(cv::Mat &mat);
-cv::Mat QImage2Mat_with_pointer(QImage &image);
+// ==================================================================================+
+// Warning!
+// When the src Mat channels is 3 or the src QImage format is RGB,
+// the Mat2QImage_with_pointer and QImage2Mat_with_pointer will change the src image.
+// When use Mat2QImage_with_pointer or Mat2QImage_with_pointer,
+// the src Mat::data (src QImage::bits()) address
+// and dst QImage::bits() (dst Mat::data) address are the same!
+// ===================================================================================
+QImage Mat2QImage_with_pointer(cv::Mat &src);
+cv::Mat QImage2Mat_with_pointer(QImage &src);
 
-QImage Mat2QImage_with_data(cv::Mat &mat);
-cv::Mat QImage2Mat_with_data(QImage &image);
+QImage Mat2QImage_with_data(cv::Mat &src);
+cv::Mat QImage2Mat_with_data(QImage &src);
 
 #endif // MAT_QIMAGE_CONVERT_H
